ae093fb49532fa7dd25198ce385542853943a9bc
Aryan Kumar Prasad <aryankp2001@gmail.com>
2022-11-16 23:25:06 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+      host: 'localhost',
+      user: 'root',
+      password: 'Aiagbitc2001@',
+      database: 'miniproject'
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
